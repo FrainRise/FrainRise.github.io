99d8055b8e19b9ded1ba63cb40ee3afb26d05ed8
Andrew Kovalenko <32896773+FrainRise@users.noreply.github.com>
2021-04-08 20:18:37 +0300

    Delete drop-landing directory

--- drop-landing/js/main.js
+++
@@ -1,9 +0,0 @@
-let btnContainer = document.getElementById('btnContainer');
-let navMenu = document.getElementById('navMenu')
-
-btnContainer.addEventListener('click', () => {
-    btnContainer.classList.toggle('clicked')
-    navMenu.classList.toggle('shown')
-
-    console.log(btnContainer)
-})
