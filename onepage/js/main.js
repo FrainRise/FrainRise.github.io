7970d3189e759afd6386faecfcaec519741c2bdc
Andrew Kovalenko <32896773+FrainRise@users.noreply.github.com>
2021-04-16 11:22:48 +0300

    Add files via upload

--- onepage/js/main.js
+++ onepage/js/main.js
@@ -4,7 +4,7 @@ let navWrapper = document.getElementById('customHeader')
 let stickyNav = navWrapper.offsetTop
 
 let scrollCallback = window.addEventListener('scroll', () => {
-    if(window.pageYOffset >= stickyNav) {
+    if(window.pageYOffset > stickyNav) {
         navWrapper.classList.add('sticky')
     } else {
         navWrapper.classList.remove('sticky')
@@ -14,14 +14,31 @@ let scrollCallback = window.addEventListener('scroll', () => {
 // Nav links active class
 
 let links = document.querySelectorAll('.list-link a')
+let hrefSections = document.getElementsByClassName('href-section')
+console.log(hrefSections)
 
+window.addEventListener('scroll', () => {
+    let index = hrefSections.length
+    
+    while(--index && window.scrollY + 50 < hrefSections[index].offsetTop){}
+    
+    links.forEach(link => {
+        link.classList.remove('active')
+    })
+    
+    links[index].classList.add('active')    
+})
+
+/*
 links.forEach(link => {
     link.addEventListener('click', () => {
         links.forEach(lnk => lnk.classList.remove('active'))
         link.classList.add('active')
     })
 
-})
+})*/
+
+
 // Smooth scrolling to sections
 
 let anchorLinks = document.querySelectorAll('a[href*="#"]')
@@ -65,7 +82,6 @@ let sections = document.getElementsByClassName('section')
 
 function showPages() {
     loaderSection.remove()
-    console.log(sections)
     for(let section of sections) {
         section.classList.remove('hidden-section')
     }
@@ -75,3 +91,28 @@ function showPages() {
 function loaderDelay() {
     delay = setTimeout(showPages, 2000)
 }
+
+
+/* Sending an info */
+let form = document.getElementById('contactForm')
+
+function validateEmail(e) {
+    e.preventDefault();
+    
+    let usernameField = document.getElementById('username')
+    let usermailField = document.getElementById('usermail')
+    let messageField = document.getElementById('message')
+    let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if(usermailField.value.match(regex) && usernameField !== '' && messageField !== '') {
+        alert('Thank you! We will contact with you soon')
+
+        usernameField.value = ''
+        usermailField.value = ''
+        messageField.value = ''
+    } else {
+        alert('Something goes wrong... Fill all fields or check your email address once again')
+    }
+}
+
+form.addEventListener('submit', validateEmail)
